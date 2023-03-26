4b1c9d3726d4c62518d027c3a566d616185b9c53
Arthur Viegas <arthurpviegas@gmail.com>
2023-03-26 03:59:43 -0300

    Adição de campos + estilização

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,6 @@ const parseCSV = (text, setTabela) => {
   let tabela = new Tabela(result.header, result.data);
   setTabela(tabela);
 
-  //console.log(result.header.indexOf("BRASILIA"));
-
   return result;
 };
 
@@ -72,10 +70,22 @@ function App() {
         </div>
       </nav>
 
+
+
+
+
+      
+
+
+
+
+
+
+
       <div className="container">
         <div className="row">
           <div className="col mb-3">
-            <h1 className="display-4">Sistema de transporte de cargas</h1>
+            <h1 className="display-4 titulo">Sistema de transporte de cargas</h1>
           </div>
         </div>
 
@@ -155,7 +165,6 @@ function App() {
           </div>
         </div>
 
-
         <div className="row">
           <div className="col mb-3">
             <h2 className="display-5">Consultar trecho x modalidade</h2>
@@ -192,7 +201,18 @@ function App() {
 
         <div className="row mb-4">
           <div className="col-md-4">
-            <button type="button" className="btn btn-primary" onClick={() => procuraTrecho(document.getElementById('cidade1').value, document.getElementById('cidade2').value, document.getElementById('tipoCaminhao').value, tabela)}>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() =>
+                procuraTrecho(
+                  document.getElementById("cidade1").value,
+                  document.getElementById("cidade2").value,
+                  document.getElementById("tipoCaminhao").value,
+                  tabela
+                )
+              }
+            >
               Consultar trecho
             </button>
           </div>
@@ -205,14 +225,214 @@ function App() {
         </div>
 
         <div className="row">
+          {/* Itens iniciais */}
+          <div className="col mb-3">
+            <h6>Itens iniciais</h6>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Itens</th>
+                  <th>Quantidade (unidades)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Celular</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoCelular"
+                      pattern="[0-9]*"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Geladeira</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoGeladeira"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Freezer</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoFreezer"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Cadeira</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoCadeira"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Luminária</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoLuminaria"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Lavadora de roupas</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoLavadora"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          {/* Trajeto da entrega */}
+          <div className="col mt-4">
+            <div className="mt-6"></div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Partida"
+              id="parada1"
+            />
+            <input
+              type="text"
+              className="form-control mt-5 mb-5"
+              placeholder="Parada"
+              id="parada2"
+            />
+            <input
+              type="text"
+              className="form-control mt-5"
+              placeholder="Destino"
+              id="parada3"
+            />
+
+            <div>
+              <button type="button" className="mt-5 btn btn-dark">Fazer entrega</button>
+            </div>
+          </div>
+
+          {/* Intes para descarregar */}
+          <div className="col mb-3">
+            <h6>Descarregar na parada</h6>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Itens</th>
+                  <th>Quantidade (unidades)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Celular</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoCelular"
+                      pattern="[0-9]*"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Geladeira</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoGeladeira"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Freezer</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoFreezer"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Cadeira</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoCadeira"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Luminária</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoLuminaria"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>Lavadora de roupas</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control ocupando"
+                      id="pesoLavadora"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          {/* Exibir resultado */}
+          <div className="col mb-3"></div>
+        </div>
+
+        <div className="row">
           <div className="col mb-3">
             <h2 className="display-5">Dados estatísticos</h2>
           </div>
         </div>
 
         <div className="row">
+          <div className="col-md-4">
+            <button type="button" className="btn btn-success mb-5">
+              Exibir dados estatísticos
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
           <div className="col mb-3">
             <h2 className="display-5">Finalizar programa</h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <button type="button" className="btn btn-danger">
+              Limpar histórico
+            </button>
           </div>
         </div>
       </div>
