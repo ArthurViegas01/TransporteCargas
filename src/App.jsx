92b8ee75a9ece8148e42020f91364861ae2dd92e
Arthur Viegas <arthurpviegas@gmail.com>
2023-03-28 21:56:03 -0300

    Tentativa de fix do package.json

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import { Tabela, procuraTrecho, cadastraTransporte } from "./Sistema.js";
 import logo from "./delllogo.png";
 import DataGrid from "./components/DataGrid";
 import Tables from "./components/Tables";
-import Caminhao1 from "./assets/caminhao1.png";
-import Caminhao2 from "./assets/caminhao2.png";
-import Caminhao3 from "./assets/caminhao3.png";
-
+import Carousel from "./components/Carousel";
 import "./App.css";
-import "./carousel.css";
+
 
 const parseCSV = (text, setTabela) => {
   const result = {
@@ -76,54 +73,7 @@ function App() {
         </div>
       </nav>
 
-      <div className="divTesteNave">
-        <div className="justify-content-center">
-          <div id="carouselExampleIndicators" className="carousel slide" data-ride="false" data-interval="false">
-            <ol className="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img src={Caminhao3} className="" alt="..."></img>
-                <div className="container">
-                  <div className="carousel-caption text-left">
-                    <h1>Dely.</h1>
-                    <p>A transportadora que você pode confiar.</p>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={Caminhao2} className="" alt="..."></img>
-                <div className="container">
-                  <div className="carousel-caption text-start">
-                    <h1>Alcance</h1>
-                    <p>Serviços de entrega com disponibilidade para todos os estados do Brasil</p>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <img src={Caminhao1} className="" alt="..."></img>
-                <div className="">
-                  <div className="carousel-caption text-right">
-                    <h1>Inclusão</h1>
-                    <p>Website com design feito com design responsivo para você acessar de qualquer lugar</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="sr-only">Anterior</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="sr-only">Próximo</span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Carousel/>
 
       <div className="container">
         <div className="row">
@@ -149,7 +99,7 @@ function App() {
         <div className="row">
           <div className="col mb-3">
             <h2 className="display-5">Consultar trecho x modalidade</h2>
-            <p>Por favor digitar sem utilizar acentos</p>
+            <p>&#40;Por favor digitar sem utilizar acentos&#41;</p>
           </div>
         </div>
 
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Caminhao1 from "../assets/caminhao1.png";
+import Caminhao2 from "../assets/caminhao2.png";
+import Caminhao3 from "../assets/caminhao3.png";
+import "./carousel.css";
+
+export const Carousel = () => {
+  return (
+    <div className="divTesteNave">
+        <div className="justify-content-center">
+          <div id="carouselExampleIndicators" className="carousel slide" data-ride="false" data-interval="false">
+            <ol className="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src={Caminhao3} className="" alt="..."></img>
+                <div className="container">
+                  <div className="carousel-caption text-left">
+                    <h1>Dely.</h1>
+                    <p>A transportadora que você pode confiar.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={Caminhao2} className="" alt="..."></img>
+                <div className="container">
+                  <div className="carousel-caption text-start">
+                    <h1>Alcance</h1>
+                    <p>Serviços de entrega com disponibilidade para todos os estados do Brasil</p>
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={Caminhao1} className="" alt="..."></img>
+                <div className="">
+                  <div className="carousel-caption text-right">
+                    <h1>Inclusão</h1>
+                    <p>Website com design feito com design responsivo para você acessar de qualquer lugar</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Anterior</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Próximo</span>
+            </button>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Carousel;
